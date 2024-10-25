2a0260ede499c63a67c1419209066b413104d813
Alessia Tagliaferri <alessia.tagliaferri@mail.polimi.it>
2024-10-25 18:34:55 +0200

    change repository

--- sketch.js
+++ sketch.js
@@ -1,17 +1,55 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  noLoop();
+  
 }
+
+let nLines = 100;
 
 function draw() {
   // put drawing code here
-}
+  background("#b6c2cd");
+  let colors = ["#161a1e", "#182028", "#222930", "#091018"];
+  //fill('#161a1e');
+  noStroke(); 
+  let lato = 20;
+  let distanza = lato * sqrt(2);
+  let margine = 30;
+
+  
+  let nColums = floor((windowWidth - 2 * margine)/distanza); //lato*sqrt2
+  let nRows = (windowHeight - 2* margine)/distanza;
+ 
+  rectMode(CENTER);
+  
+  //if qualcosa --> creo questo, else --> ruoto di QUARTER_PI
+  //qualcosa: se  100 è divisibile per 3
+
+  for(let i = 0; i < nColums; i++) { //ciclo per fare le colonne
+
+    for(let j = 0; j < nRows; j++) { //ciclo per fare le righe
+      
+      let x = i * distanza + margine + distanza / 2 ;
+      let y = j * distanza + margine + distanza / 2 ;
+      
+      push();
+      translate(x, y);
+      
+      let q = random(0, 10);
+
+      if(q < 7) {
+        rotate(QUARTER_PI);
+        
+      }
+
+      let color = random(colors);
+      fill(color)
+      rect(0, 0, lato, lato); 
+      pop();
+    }
+    
+  }
+
+} 
+{}
